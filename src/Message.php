8955af54ff614678c3e81220e311324b4896c947
luklewluk <luklewluk@gmail.com>
2015-03-15 19:04:48 +0100

    Update Message.php
    NullLogger if logger is not given as an argument

--- src/Message.php
+++ src/Message.php
@@ -37,9 +37,10 @@ final class Message
      *
      * @throws WebSocketException
      */
-    public function __construct(LoggerInterface $logger, DataFrame $frame = null)
+    public function __construct(LoggerInterface $logger = null, DataFrame $frame = null)
     {
-        $this->logger = $logger;
+        $this->logger = ($logger === null) ? new NullLogger() : $logger;
+        $this->router = ($router === null) ? new HttpRouter($this->logger) : $router;
 
         if ($frame === null) {
             $this->isComplete = true;
